00d173eeae42280c1350dc325ac836254ace3f9c
Mathías Ezequiel Latrónico <mathiezelat@gmail.com>
2025-12-26 11:51:43 +0000

    Change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://mathxlat.github.io",
-  base: "/portfolio",
+  site: "https://portfolio.mathxlat.dev",
+  // base: "/",
 });
